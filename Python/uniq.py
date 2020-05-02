7f6ee161fe5f21736f0eb3449d3dfa5be618bbe2
divigarg <divyansh.gargsg@gmail.com>
2020-05-02 11:44:33 +0530

    Changed the logic in uniq.py

--- Python/uniq.py
+++ Python/uniq.py
@@ -2,12 +2,14 @@ import argparse
 import os, sys
 #repeat_check func
 def repeat_check(index,lines):
-    try:
-        if lines[index].rstrip()==lines[index+1].rstrip(): 
-            return True
-    except:
-        pass
-    return False
+    count=0
+    for i in range(index,len(lines)):
+        if lines[i].rstrip()==lines[index].rstrip():
+            count+=1
+        else:
+            return count,i
+    return count,len(lines)
+
 
 #printing_line definiition
 def printing_line(index,lines,counting,count):
@@ -18,22 +20,33 @@ def printing_line(index,lines,counting,count):
     sys.stdout.write(result)
 
 #read lines file
-def read_line(input_file,counting):
+def read_line(input_file,counting,only_dup,all_dup):
     filepath = sys.stdin if input_file=='standard input' else open(input_file,'r')
     lines=filepath.readlines()
-    count=1
-    for index in range(len(lines)):
-        if not repeat_check(index,lines):
+    index=0
+    while True:
+        if index==len(lines):
+            break
+        count,new_index=repeat_check(index,lines)
+        if only_dup:
+            if count==1:
+                index=new_index
+                continue
+        no_times=1
+        if all_dup:
+            no_times=count
+        for _ in range(no_times):
             printing_line(index,lines,counting,count)
-            count=1
-        else:
-            count+=1
-    print()
+        index =new_index
 #setup_parser
 def setup_parser():
     parser=argparse.ArgumentParser(prog='uniq',description='Filter adjacent matching lines from INPUT (or standard input)'\
-        'writing to output (or standard output)')
+        'writing to output (or standard output)',
+        usage='uniq [OPTIONS]... [INPUT]')
+
     parser.add_argument('-c','--count',action='store_true',help='prefix lines with the no of occurence')
+    parser.add_argument('-d','--repeated',action='store_true',help='only prints duplicate lines, one for each group')
+    parser.add_argument('-D',action='store_true',help='print all duplicate lines')
     parser.add_argument('input',metavar='INPUT',action='store',type=str,default='-',nargs='?',help='the file(s) to handle')
     return parser
 #main func
@@ -44,6 +57,12 @@ def main():
     if input_file=='-':
         input_file='standard input'
     counting=args.count
-    read_line(input_file,counting)
+    only_dup=args.repeated or args.D
+    all_dup= args.D
+    if counting and all_dup:
+        print('uniq: printing all duplicate lines and repeat counts is meaningless')
+        print("try python 'uniq.py --help' for more help")
+        exit(-1)
+    read_line(input_file,counting,only_dup,all_dup)
 
 main()
