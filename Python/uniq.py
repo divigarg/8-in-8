19a9d35c340d4634c2fc77f6f766907751e871a7
divigarg <divyansh.gargsg@gmail.com>
2020-05-02 12:26:47 +0530

    uniq.py updated with -s -u

--- Python/uniq.py
+++ Python/uniq.py
@@ -1,10 +1,15 @@
 import argparse
 import os, sys
 #repeat_check func
-def repeat_check(index,lines):
+def repeat_check(index,lines,icase,Nchars):
     count=0
     for i in range(index,len(lines)):
-        if lines[i].rstrip()==lines[index].rstrip():
+        line_1=lines[i][Nchars:].rstrip()
+        line_2=lines[index][Nchars:].rstrip()
+        if icase:
+            line_1=line_1.lower()
+            line_2=line_2.lower()
+        if line_1==line_2:
             count+=1
         else:
             return count,i
@@ -20,23 +25,27 @@ def printing_line(index,lines,counting,count):
     sys.stdout.write(result)
 
 #read lines file
-def read_line(input_file,counting,only_dup,all_dup):
+def read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup):
     filepath = sys.stdin if input_file=='standard input' else open(input_file,'r')
     lines=filepath.readlines()
     index=0
     while True:
         if index==len(lines):
             break
-        count,new_index=repeat_check(index,lines)
+        count,new_index=repeat_check(index,lines,icase,Nchars)
         if only_dup:
             if count==1:
+                index=new_index
+                continue
+        if only_nondup:
+            if count!=1:
                 index=new_index
                 continue
         no_times=1
         if all_dup:
             no_times=count
-        for _ in range(no_times):
-            printing_line(index,lines,counting,count)
+        for i in range(index,index+no_times):
+            printing_line(i,lines,counting,count)
         index =new_index
 #setup_parser
 def setup_parser():
@@ -45,8 +54,12 @@ def setup_parser():
         usage='uniq [OPTIONS]... [INPUT]')
 
     parser.add_argument('-c','--count',action='store_true',help='prefix lines with the no of occurence')
-    parser.add_argument('-d','--repeated',action='store_true',help='only prints duplicate lines, one for each group')
+    d_u=parser.add_mutually_exclusive_group()
+    d_u.add_argument('-d','--repeated',action='store_true',help='only prints duplicate lines, one for each group')
+    d_u.add_argument('-u','--unique',action='store_true',help='only prints unique lines')
     parser.add_argument('-D',action='store_true',help='print all duplicate lines')
+    parser.add_argument('-i','--ignore-case',action='store_true',help='ignore differences in case when comparing')
+    parser.add_argument('-s','--skip-chars',metavar='N',action='store',default=0,type=int,help='avoid comparing the first N chars')
     parser.add_argument('input',metavar='INPUT',action='store',type=str,default='-',nargs='?',help='the file(s) to handle')
     return parser
 #main func
@@ -58,11 +71,20 @@ def main():
         input_file='standard input'
     counting=args.count
     only_dup=args.repeated or args.D
+    only_nondup=args.unique
     all_dup= args.D
+    Nchars=args.skip_chars
+    icase=args.ignore_case
+    
+    if counting and only_nondup:
+        print('uniq: printing only unique lines and repeat counts is meaningless')
+        print("try 'python uniq.py --help' for more help")
+        exit(-1)
+    
     if counting and all_dup:
         print('uniq: printing all duplicate lines and repeat counts is meaningless')
-        print("try python 'uniq.py --help' for more help")
+        print("try 'python uniq.py --help' for more help")
         exit(-1)
-    read_line(input_file,counting,only_dup,all_dup)
+    read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup)
 
 main()
