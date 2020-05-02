e6e7b22f52e0b718b0eadf609b33b1f0475fad68
divigarg <divyansh.gargsg@gmail.com>
2020-05-02 11:55:50 +0530

    find.py

--- Python/find.py
+++ Python/find.py
@@ -1,7 +1,2 @@
 import sys,os
 import argparse
-parser=argparse.ArgumentParser(prog='find',description='To find all the files with specific property')
-args=parser.parse_args()
-path='.'
-f = []
-print(list(os.walk(path)))
