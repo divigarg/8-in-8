850f8d504bf6a03f43227ea937661b28231fff13
divigarg <divyansh.gargsg@gmail.com>
2020-05-03 16:51:46 +0530

    the flag -f for skipping fields is added to the uniq.py file

--- Python/uniq.py
+++ Python/uniq.py
@@ -2,7 +2,7 @@ import argparse
 import os, sys
 #repeat_check func
 def repeat_check(index,lines,icase,Nchars):
-    count=0
+    count=0 
     for i in range(index,len(lines)):
         line_1=lines[i][Nchars:].rstrip()
         line_2=lines[index][Nchars:].rstrip()
@@ -25,14 +25,31 @@ def printing_line(index,lines,counting,count):
     sys.stdout.write(result)
 
 #read lines file
-def read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup):
+def read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup,skip_fields):
     filepath = sys.stdin if input_file=='standard input' else open(input_file,'r')
     lines=filepath.readlines()
     index=0
+    new_lines=[]
+    for line in lines:
+        words=line.split(' ')
+        skip=0
+        index_word=0
+        for word in words:
+            if skip==skip_fields:
+                new_line=" "+" ".join(words[index_word:])
+                new_lines.append(new_line)
+                skip+=1
+                break
+            if word!='':
+                skip+=1 
+            index_word+=1
+        if skip<skip_fields+1:
+            new_lines.append('')
+        
     while True:
         if index==len(lines):
             break
-        count,new_index=repeat_check(index,lines,icase,Nchars)
+        count,new_index=repeat_check(index,new_lines,icase,Nchars)
         if only_dup:
             if count==1:
                 index=new_index
@@ -57,7 +74,9 @@ def setup_parser():
     d_u=parser.add_mutually_exclusive_group()
     d_u.add_argument('-d','--repeated',action='store_true',help='only prints duplicate lines, one for each group')
     d_u.add_argument('-u','--unique',action='store_true',help='only prints unique lines')
-    parser.add_argument('-D',action='store_true',help='print all duplicate lines')
+    parser.add_argument('-D','--all-repeated',action='store_true',help='print all duplicate lines')
+    # parser.add_argument('--all-repeated',action='store',metavar='=METHOD',choices=['none','seperate','prepend'])
+    parser.add_argument('-f','--skip-fields',help='avoid comparing the first N fields',metavar='N',type=int,default=0)
     parser.add_argument('-i','--ignore-case',action='store_true',help='ignore differences in case when comparing')
     parser.add_argument('-s','--skip-chars',metavar='N',action='store',default=0,type=int,help='avoid comparing the first N chars')
     parser.add_argument('input',metavar='INPUT',action='store',type=str,default='-',nargs='?',help='the file(s) to handle')
@@ -70,12 +89,12 @@ def main():
     if input_file=='-':
         input_file='standard input'
     counting=args.count
-    only_dup=args.repeated or args.D
+    only_dup=args.repeated or args.all_repeated
     only_nondup=args.unique
-    all_dup= args.D
+    all_dup= args.all_repeated
     Nchars=args.skip_chars
     icase=args.ignore_case
-    
+    skip_fields=args.skip_fields
     if counting and only_nondup:
         print('uniq: printing only unique lines and repeat counts is meaningless')
         print("try 'python uniq.py --help' for more help")
@@ -85,6 +104,6 @@ def main():
         print('uniq: printing all duplicate lines and repeat counts is meaningless')
         print("try 'python uniq.py --help' for more help")
         exit(-1)
-    read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup)
+    read_line(input_file,counting,only_dup,all_dup,icase,Nchars,only_nondup,skip_fields)
 
 main()
